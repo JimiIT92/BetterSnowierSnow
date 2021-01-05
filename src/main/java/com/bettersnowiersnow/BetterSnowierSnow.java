3d422287336221d3e40b3a5ce3b7e28abc922c45
jimiit92 <jimiit92@gmail.com>
2021-01-05 13:04:33 +0100

    First commit

---
+++ src/main/java/com/bettersnowiersnow/BetterSnowierSnow.java
@@ -0,0 +1,16 @@
+package com.bettersnowiersnow;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterSnowierSnow extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
